bd5742f028888a7376764d29cd42ef3eae049d79
shantoo <mrahman7148@gmail.com>
2025-10-28 15:45:51 +0600

    added recipes card in home page

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 import {Home} from './pages/home.jsx';
 import {createBrowserRouter, RouterProvider} from 'react-router';
 import axios from 'axios';
+import {MainNavigation} from './components/mainNavigation.jsx';
 
-const getAllRecipes=async()=>{
-  let allRecipes=[]
-  await axios.get('http://localhost:5000/recipe').then(res=>{
-    allRecipes=res.data
-  })
-  return allRecipes
+const getAllRecipes = async () => {
+  const res = await axios.get('http://localhost:5000/recipe')
+  return res.data.data
 }
 
 const router = createBrowserRouter([
 
-      {path:"/",element:<Home/>}
+  {path:"/",element:<MainNavigation/>,children:[
+      {path:"/",element:<Home/>,loader: getAllRecipes}
+    ]}
 
 ])
 export const App = () => {
--- frontend/src/components/mainNavigation.jsx
+++ frontend/src/components/mainNavigation.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Navbar} from './navbar.jsx';
 import {Footer} from './footer.jsx';
+import {Outlet} from 'react-router';
 
 export const MainNavigation = () => {
   return (
